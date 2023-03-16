9c612603be3952e0f8851f55cb755ed2847d5a1b
Thomasperge <thomaskauffmant74@gmail.com>
2023-03-16 10:43:57 +0100

    feat: add mongoose to connect mongoDB

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 let windows
 
@@ -23,29 +23,14 @@ const createWindow = () => {
     windows.loadFile('src/pages/index.html')
 }
 
-// Exemple d'utilisation de la fonction de connexion à MongoDB
 app.on('ready', async () => {
-    // Replace the placeholders with your credentials and hostname
-    const uri = 'mongodb+srv://Thomasperge:RonnieColeman74360-@flowly.wyde8da.mongodb.net/?retryWrites=true&w=majority';
-    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-    const client = new MongoClient(uri,  {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    });
-    
-    try {
-        // Connect the client to the server (optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-      } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-      }
+    const uri = process.env.MONGODB_HOST;
+
+    mongoose.connect(uri, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log('MongoDB : Ready !'));
 });
 
 app.whenReady().then(() => {
