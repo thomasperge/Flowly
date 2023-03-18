14721c74cac543df760b4682f7c0070dc12f5eb0
Thomasperge <thomaskauffmant74@gmail.com>
2023-03-18 23:24:06 +0100

    feat: register page done

---
+++ src/components/script/register.js
@@ -0,0 +1,9 @@
+var individualSelector = document.getElementById('signupCheckoxIndividual')
+var radioIndividual = document.getElementById('radioIndividual')
+
+var compagnieSelector = document.getElementById('signupCheckoxCompagnie')
+var radioCompagnie = document.getElementById('radioCompagnies')
+
+
+individualSelector.addEventListener('click', () => {
+})
