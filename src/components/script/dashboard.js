81fd3abbc335858ee1ff7688cf19ed610a48d853
Thomasperge <thomaskauffmant74@gmail.com>
2023-03-20 01:25:52 +0100

    fea: change size metric

--- src/components/script/dashboard.js
+++ src/components/script/dashboard.js
@@ -1,13 +1,23 @@
 let button = document.getElementById('toggle-button')
+let testArea = document.getElementById('testArea')
 
-button.addEventListener('click', (event) => {
-    console.log("HERE");
+testArea?.addEventListener('transitionend', () => {
+    console.log("HERRRRRRRRRRRRE");
+    if (testArea.style.opacity === "0") {
+      testArea.style.display = "none";
+    }
+});
 
+button?.addEventListener('click', (event) => {
     if (button.classList.contains('fa-circle-plus')) {
         button.classList.remove('fa-circle-plus');
         button.classList.add('fa-circle-minus');
+        testArea.style.display = "flex"
+        testArea.style.opacity = 1;
     } else {
         button.classList.remove('fa-circle-minus');
         button.classList.add('fa-circle-plus');
+        testArea.style.display = "none"
+        testArea.style.opacity = 0;
     }
 })
