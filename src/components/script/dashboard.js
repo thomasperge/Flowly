cfaab3a0b31f10a903e30ffeb8b067e88110afdc
Thomasperge <thomaskauffmant74@gmail.com>
2023-03-21 13:22:56 +0100

    feat: add button to add final version

--- src/components/script/dashboard.js
+++ src/components/script/dashboard.js
@@ -1,70 +1,60 @@
 let button = document.getElementById('toggle-button')
-let testArea = document.getElementById('testArea')
 let containerDashBoard2 = document.getElementById('dashboard-SCountainer2-2')
-
-let carButton = document.getElementById('fa-car')
-let boltButton = document.getElementById('fa-bolt')
-let addTransport = document.getElementById('dashboard-AddTransportArea')
+let addArea = document.getElementById("addArea")
 
 let carAddDiv = false;
 let energyAddDiv = false;
 
 button?.addEventListener('click', (event) => {
     if (button.classList.contains('fa-circle-plus')) {
+        addArea.style.display = "flex"
+
         button.classList.remove('fa-circle-plus');
         button.classList.add('fa-circle-minus');
-        testArea.style.display = "flex"
 
+        button.style.transform = "translateX(-230%)";
+        button.style.transition = "transform 0.5s";
+
+        setTimeout(() => {
+            addArea.style.transform = "translateX(-60%)";
+            addArea.style.transition = "transform 0.5s";
+        }, 10);
     } else {
         button.classList.remove('fa-circle-minus');
         button.classList.add('fa-circle-plus');
-        testArea.style.display = "none"
-    }
 
-    // A revoie car pas sur
-    carAddDiv = false;
-    energyAddDiv = false;
-    document.getElementById('dashboard-AddEnergyArea').style.display = "none"
-    document.getElementById('dashboard-AddTransportArea').style.display = "none"
+        button.style.transform = "translateX(0%)";
+        button.style.transition = "transform 0.5s";
+
+        setTimeout(() => {
+            addArea.style.transform = "translateX(0%)";
+            addArea.style.transition = "transform 0.5s";
+        }, 4);
+
+        addArea.style.display = "none"
+    }
 })
+
+addArea.addEventListener('transitionend', () => {
+    if (button.classList.contains('fa-circle-plus')) {
+        addArea.style.display = "none"
+    } else {
+        addArea.style.display = "flex"
+    }
+})
+
 
 containerDashBoard2?.addEventListener('click', (event) => {
+    if (button.classList.contains('fa-circle-minus')) {
+        button.style.transform = "translateX(0%)";
+        button.style.transition = "transform 0.5s";
+
+        addArea.style.transform = "translateX(0%)";
+        addArea.style.transition = "transform 0.5s";
+
+        addArea.style.display = "none"
+    }
+
     button.classList.remove('fa-circle-minus');
     button.classList.add('fa-circle-plus');
-    testArea.style.display = "none"
-
-    // A revoie car pas sur
-    carAddDiv = false;
-    energyAddDiv = false;
-    document.getElementById('dashboard-AddEnergyArea').style.display = "none"
-    document.getElementById('dashboard-AddTransportArea').style.display = "none"
-
-})
-
-carButton?.addEventListener('click', (event) => {
-    // Delete Bolt Add
-    document.getElementById('dashboard-AddEnergyArea').style.display = "none"
-    energyAddDiv = false;
-
-    if (carAddDiv) {
-        document.getElementById('dashboard-AddTransportArea').style.display = "none"
-        carAddDiv = false;
-    } else {
-        document.getElementById('dashboard-AddTransportArea').style.display = "block"
-        carAddDiv = true;
-    }
-})
-
-boltButton?.addEventListener('click', (event) => {
-    // Delete Car Add
-    document.getElementById('dashboard-AddTransportArea').style.display = "none"
-    carAddDiv = false;
-
-    if (energyAddDiv) {
-        document.getElementById('dashboard-AddEnergyArea').style.display = "none"
-        energyAddDiv = false;
-    } else {
-        document.getElementById('dashboard-AddEnergyArea').style.display = "block"
-        energyAddDiv = true;
-    }
 })
