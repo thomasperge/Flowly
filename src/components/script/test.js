5ce77e893a14c50b4b3686e9e20c6f5120e04c30
Thomasperge <thomaskauffmant74@gmail.com>
2023-03-24 00:33:33 +0100

    feat: remake convert function

--- src/components/script/test.js
+++ src/components/script/test.js
@@ -1,21 +1,27 @@
-const solds = 1250; 
-
-const number = (solds >= 1000000000 && solds < 1000000000000) ? { divisor: 1000000000, postfix: "B" }: (solds >= 1000000 && solds < 1000000000) ? { divisor: 1000000, postfix: "M" }: (solds >= 1000 && solds < 1000000) ? { divisor: 1000, postfix: "K" }: { divisor: 1, postfix: null }; 
-
-const floor = Math.floor(solds / number.divisor).toLocaleString();
-const firstDecimalIndex = solds.toLocaleString().charAt(floor.length+1);
-const secondDecimalIndex = solds.toLocaleString().charAt(floor.length+2);
-
-const after1 = (firstDecimalIndex.length > 0) ? firstDecimalIndex : 0
-const after2 = (secondDecimalIndex.length > 0) ? secondDecimalIndex : 0
-const afterPrefix = (number.postfix != null) ? number.postfix : 0
-
-let final
-
-if (after1 != 0 && after2 != 0 && afterPrefix != 0) {
-    final = firstDecimalIndex.match("0")? floor + number.postfix: floor + "." + after1 + after2 + number.postfix; 
-} else {
-    final = solds
+function formatNumber(number) {
+    const prefixes = ['', 'K', 'M', 'B'];
+    const base = 1000;
+  
+    const isNegative = number < 0;
+    number = Math.abs(number);
+  
+    if (number < base) {
+      return isNegative ? '-' + number : number.toString();
+    }
+  
+    const log = Math.floor(Math.log10(number) / Math.log10(base));
+    const prefix = prefixes[log];
+  
+    const value = number / Math.pow(base, log);
+  
+    const formattedValue = value.toFixed(2);
+  
+    return (isNegative ? '-' : '') + formattedValue + prefix;
 }
 
-console.log(final);
+console.log(
+    formatNumber(125),
+    formatNumber(2500000),
+    formatNumber(4500000000),
+    formatNumber(-75000),
+);
