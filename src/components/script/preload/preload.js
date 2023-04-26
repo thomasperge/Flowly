cc7addbb3272a6e65054fcdaed735e36e25e5cab
Thomasperge <thomaskauffmant74@gmail.com>
2023-04-26 23:18:55 +0200

    feat: add employee name

--- src/components/script/preload/preload.js
+++ src/components/script/preload/preload.js
@@ -263,8 +263,7 @@ ipcRenderer.on('database/send-all-history', (event, result) => {
                         <span class="history-areaContainerDate flex">${formatDate(result[i]._doc.dateInput)}</span>
                     </div>
                     <div class="history-areaContainerOverview">
-                            <span class="history-areaContainerDesc flex">${result[i]._doc.description_record}</span>
-                    <span class="history-areaContainerDesc flex">${result[i]._doc.nameEmployee}</span>
+                        <span class="history-areaContainerDesc flex">${result[i]._doc.description_record} - ${result[i]._doc.nameEmployee}</span>
                     </div>
                     <div class="history-areaContainerData flex">
                         <div class="history-areaContainerDataKm flex">${result[i]._doc.string_value}</div>
